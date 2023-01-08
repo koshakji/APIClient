c9aa56052d1a3775b55a3f1ed528231d34f744ae
Majd Koshakji <koshakji@gmail.com>
2023-01-08 19:21:11 +0100

    Added initial documentation for `BaseClient`

--- Sources/APIClient/ClientProtocols/BaseClient.swift
+++ Sources/APIClient/ClientProtocols/BaseClient.swift
@@ -7,15 +7,39 @@
 
 import Foundation
 
+
+/// The protocol defining the base of an API client.
 public protocol BaseClient {
+    
+    /// Encoder type.
     associatedtype Encoder: EncoderProtocol where Encoder.Output == Data
+    
+    /// Decoder type.
     associatedtype Decoder: DecoderProtocol where Decoder.Input == Data
+    
+    
+    /// Encoder used for encoding HTTP request body into `Data`.
     var encoder: Encoder { get }
+    
+    /// Decoder used for decoding HTTP responses into objects.
     var decoder: Decoder { get }
+    
+    /// URLSession used to make all requests.
     var session: URLSession { get }
     
+    
+    /// Creates a `URLRequest` from an endpoint, body and headers
+    /// - Parameters:
+    ///   - endpoint: The request definition (includes URL, URL parameters and method).
+    ///   - body: The request body, must match the body type of the request.
+    ///   - headers: The request HTTP headers, must match the headers type of the request.
+    /// - Returns: The `URLRequest` used to make the actual request.
     func createURLRequest<Request: RequestProtocol>(endpoint: Request, body: Request.Body, headers: Request.Headers) throws -> URLRequest
     
+    
+    /// Request customization point.
+    /// - Parameter request: final object that will be used to make the request.
+    /// Adding standard headers could be handled here.
     func customize(request: inout URLRequest)
 }
 
